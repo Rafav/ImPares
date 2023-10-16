b12dcc8c8099c1c1c59a2fede12708c8fffde845
Rafav <esi.medina@gmail.com>
2023-10-16 17:17:45 +0200

    Añadir https

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const url_base = "http://pares.mcu.es/ParesBusquedas20/ViewImage.do?accion=42&txt_zoom=10&txt_contraste=0&txt_polarizado=&txt_brillo=10.0&txt_contrast=1.0&txt_transformacion=-1&txt_descarga=1&dbCode=";
+const url_base = "https://pares.mcu.es/ParesBusquedas20/ViewImage.do?accion=42&txt_zoom=10&txt_contraste=0&txt_polarizado=&txt_brillo=10.0&txt_contrast=1.0&txt_transformacion=-1&txt_descarga=1&dbCode=";
 const url_iterator = "&txt_id_imagen=";
 
 async function downloadSequentially(dbCode, total) {
