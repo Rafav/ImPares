5c824691b9ea004968fd1c051e4b7293184d36e9
RafaV <esimedina@gmail.com>
2024-04-15 09:27:25 +0200

    Añadida validación de campos para evitar descargas en blanco. Actualizado el número de versión

--- js/popup.js
+++ js/popup.js
@@ -1,30 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startButton = document.getElementById('startButton');
-  
-    startButton.addEventListener('click', function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
-        const message = { action: 'scrapePage' };
-  
-        // Enviar un mensaje al script de contenido (content.js)
-        chrome.scripting.executeScript({
-          target: { tabId: activeTab.id },
-          function: startContentScript
-        });
+  const startButton = document.getElementById('startButton');
+
+  startButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      const message = { action: 'scrapePage' };
+
+      // Enviar un mensaje al script de contenido (content.js)
+      chrome.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        function: startContentScript
       });
     });
   });
-  
-  function startContentScript() {
-    // Este código se ejecutará en el contexto de la pestaña activa
-    // Realiza el scraping aquí y devuelve los resultados
-    const scrapedData = {}; // Realiza el scraping y guarda los resultados aquí
-  
+});
 
-    var dbCode = document.querySelector('input[name="dbCode"]').value;
-    var total = document.querySelector('input[name="txt_imagenFin"]').value;
-     
+function startContentScript() {
+  var dbCodeElement = document.querySelector('input[name="dbCode"]');
+  var totalElement = document.querySelector('input[name="txt_imagenFin"]');
+
+  if (!dbCodeElement || !totalElement) {
+    alert("No se encontraron uno o más elementos requeridos para la descarga.");
+  } else {
+    var dbCode = dbCodeElement.value;
+    var total = totalElement.value;
+
+    if (dbCode === "" || total === "") {
+      alert("el identificador del documento o el total de imágenes están vacíos.");
+      return; 
+    }
+
     // Envía los resultados de vuelta a popup.js
-    chrome.runtime.sendMessage({ action: 'scrapedData',dbCode, total });
+    chrome.runtime.sendMessage({ action: 'scrapedData', dbCode, total });
   }
-  
+}
